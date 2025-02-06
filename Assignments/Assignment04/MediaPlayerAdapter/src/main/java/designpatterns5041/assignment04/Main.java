a82dd17d1ce6983f1af3d3297cc270874ec5f9d2
odinellefsen <odin@ellefsen.fo>
2025-02-06 18:42:00 +0000

    made mp4player media player adapter

--- Assignments/Assignment04/MediaPlayerAdapter/src/main/java/designpatterns5041/assignment04/Main.java
+++ Assignments/Assignment04/MediaPlayerAdapter/src/main/java/designpatterns5041/assignment04/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         // Original
-        MediaSystem mediaSystem = new MediaSystem(
-            new OldMediaPlayer()
-        );
-        mediaSystem.playMedia();
+        // MediaSystem mediaSystem = new MediaSystem(
+        //     new OldMediaPlayer()
+        // );
+        // mediaSystem.playMedia();
 
         // Task 1
-//        MediaSystem mediaSystem2 = new MediaSystem(
-//            // Add the adapter here ...
-//        );
-//        mediaSystem2.playMedia();
+       MediaSystem mediaSystem2 = new MediaSystem(
+           new MediaPlayerAdapter()
+       );
+       mediaSystem2.playMedia();
 
         // Task 2
 //        MediaSystem mediaSystem3 = new MediaSystem(
---
+++ Assignments/Assignment04/MediaPlayerAdapter/src/main/java/designpatterns5041/assignment04/MediaPlayerAdapter.java
@@ -0,0 +1,24 @@
+package designpatterns5041.assignment04;
+
+import designpatterns5041.assignment04.newmediaplayers.Mp4Player;
+
+public class MediaPlayerAdapter implements MediaPlayer {
+    private Mp4Player mp4Player;
+
+    public MediaPlayerAdapter() {
+        mp4Player = new Mp4Player();
+    }
+
+    @Override
+    public void play(String audioType, String fileName) {
+        if (audioType.equalsIgnoreCase("mp4")) {
+            mp4Player.playMp4(fileName);
+        } else if (audioType.equalsIgnoreCase("mp3")) {
+            mp4Player.playMp3(fileName);
+        }
+        else {
+            System.out.println("Invalid media. " + audioType + " format not supported");
+        }
+    }
+
+}
